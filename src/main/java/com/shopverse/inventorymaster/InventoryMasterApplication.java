7f0c43605e0ff5efcac8acc096190961e1888451
Jace Dm <jaceverdant@gmail.com>
2025-01-04 18:14:46 +0200

    Initial commit

---
+++ src/main/java/com/shopverse/inventorymaster/InventoryMasterApplication.java
@@ -0,0 +1,13 @@
+package com.shopverse.inventorymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopverse/inventorymaster/InventoryMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopverse.inventorymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
